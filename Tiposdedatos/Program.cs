b0af778090b4378330e377a7f2f231780062ebf7
blanksss <marrophph@gmail.com>
2024-02-14 17:36:57 -0600

    primer commit

--- Tiposdedatos/Program.cs
+++ Tiposdedatos/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numero1 = 6;
+int numero2 = 18;
+int suma;
+
+suma = numero1 + numero2;
+Console.WriteLine("El resultado de la suma de dos numeros enteros es: " + suma);
+
+decimal numero3 = 9.66m;
+decimal numero4 = 5.84m;
+decimal resta;
+
+resta = numero3 - numero4;
+Console.WriteLine("El resultado de la resta de dos numeros decimales es: " + resta);
+
+float numero5 = 34.5f;
+float numero6 = 54.3f;
+float multiplicacion;
+
+multiplicacion = numero5 * numero6;
+Console.WriteLine("El resultado de la multiplicacion de dos numeros flotantes es: " + multiplicacion);
+
+int numero7 = 62;
+int numero8 = 2;
+int division;
+
+division = numero7 / numero8;
+Console.WriteLine("El resultado de la division de dos numeros es: " + division);
+
+int numero9 = 6;
+int numero10 = 2;
+int modulo;
+
+modulo = numero9 % numero10;
+Console.WriteLine("El modulo de dos numeros enteros es: " + modulo);
+
